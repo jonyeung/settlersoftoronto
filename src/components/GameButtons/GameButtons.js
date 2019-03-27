6338558037b7b52d46db197912809fda1654c108
Tony Zeng <tianyi.z3ng@gmail.com>
2019-03-27 15:06:23 -0400

    added exit button

--- src/components/GameButtons/GameButtons.js
+++ src/components/GameButtons/GameButtons.js
@@ -6,9 +6,14 @@ import styles from './GameButtons.module.css';
 class GameButtons extends Component {
   render() {
     return (
-      <div className={styles.Content}>
-        <div className={styles.TradeButton} onClick={this.props.openTradeModal}>Trade Icon</div>
-        <div className={styles.EndTurnButton} onClick={this.props.rollDice}>Roll/End Turn Icon</div>
+      <div className={styles.Container}>
+        <div className={styles.UpperContent}>
+          <div className={styles.ExitButton}>Exit</div>
+        </div>
+        <div className={styles.LowerContent}>
+          <div className={styles.TradeButton} onClick={this.props.openTradeModal}>Trade Icon</div>
+          <div className={styles.EndTurnButton} onClick={this.props.rollDice}>Roll/End Turn Icon</div>
+        </div>
       </div>
     )
   }
