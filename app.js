9846cee4cd1efbaa8a1435392e70bf9787f75740
Jonathan Yeung <jonathanmh.yeung@mail.utoronto.ca>
2019-03-30 18:30:49 -0400

    changed _id to id

--- app.js
+++ app.js
@@ -211,7 +211,7 @@ io.on('connection', function (socket) {
             gameStateRef.child(id).once('value').then(function (snapshot) {
                 let gameState = JSON.parse(snapshot.val());
                 console.log(gameState)
-                gameState._id = id;
+                gameState.id = id;
                 io.sockets.emit('PLAYER_CONNECT', JSON.stringify(gameState));
             })
 
